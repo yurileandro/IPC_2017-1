1867aaad4a6112014ccf8010afcce2bf6c7169ff
alexandreuch <alexandreuch@gmail.com>
2017-03-13 15:12:26 -0400

    Subindo a questão

---
+++ lista02/lista02_exercicio01_questao06.py
@@ -0,0 +1,24 @@
+#-----------------------------------------------------------------------------------------------------------------------
+#Introdução a Programação de Computadores - IPC
+#Universidade do Estado do Amazonas - UEA
+#Prof. Jucimar Jr.
+#Alexandre Marques Uchôa               1715310028
+#Jandinne Duarte de Oliveira           1015070265
+#Uriel Brito Barros                    1515120558
+#Roberta de Oliveira da cruz           0825070169
+#Evandro Padilha Barroso Filho         1715310009
+#
+##
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+#-----------------------------------------------------------------------------------------------------------------------
+r = float(input("Digite um raio"))
+area = (3.14*r*r)
+print ('Sua área é', area)
+
+
+
+
+
+
+
+
