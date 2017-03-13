81c1f83b7346b87493900ee654a209980c67873f
tiagoweb <tiagoweb@gmail.com>
2017-03-12 22:10:10 -0400

    removendo questão e adicionando outra

---
+++ lista02/lista02_exercicio01_questao05.py
@@ -0,0 +1,18 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Edson de Lima Barros                      1715310043
+# Gabriel Nascimento de Oliveira            1715310052
+# Luiz Daniel Raposo Nunes de Mello	        1715310049
+# Renan de Almeida Campos                   0825060036
+# Tiago Ferreira Aranha 	                1715310047
+# Wilbert Luís Evangelista Marins           1715310055
+# Mackson Garcez Moreno de Oliveira júnior  1215090300
+#
+# 1.5. Faça um Programa que converta metros para centímetros.
+# ----------------------------------------------------------
+
+metros = input('Digite a medida (em metros): ')
+centimetros = int(metros) * 100
+print (metros, 'metros são', centimetros, 'centímetros')
