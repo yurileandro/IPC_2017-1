e7925083553b38351e1e91194d66110867e0a51d
FelipeEduAlmeida <edu468felipe@gmail.com>
2017-03-14 14:42:09 -0400

    Update lista02_exercicio03.py
    Atualizando o programa. (Outro participante do meu grupo vai terminar)

--- lista02/lista02_exercicio03.py
+++ lista02/lista02_exercicio03.py
@@ -1,4 +1,20 @@
-import turtle
-colors=['red', 'purple', 'blue','green', 'yellow']
-t=turtle.Pen()
-turtle.bgcolor('black')
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Aracille de Souza Barbosa           1315120206
+# Felipe Eduardo Silva de Almeida     1715310031
+# Kethelen Tamara Braga               1525212002
+# Naiara da Silva Cedeira da Costa    1715310038
+# Vitor Summer Oliveira Pantaleão     1715310042
+# Yuri Leandro de Aquino Silva        1615100462
+#
+# Desenhar um polígono com 5 lados iguais.
+# Cada lado uma cor diferente.
+#  ----------------------------------------------------------
+
+import turtle # importa a biblioteca turtle
+colors=['red', 'purple', 'blue','green', 'yellow'] #cria um vetor de 5 posições do tipo string para cada lado do polígono
+t=turtle.Pen() #
+turtle.bgcolor('black') # muda a cor doefundo da janela para preto
+for x in range(5): # inicia um laço de 0 até 4 para a criação de cada lado do poligno
