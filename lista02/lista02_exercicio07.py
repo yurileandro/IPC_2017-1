2178e3ede550ce38b904ec3520eb3865ecb8e656
alexandreuch <alexandreuch@gmail.com>
2017-03-13 22:56:47 -0400

    Subindo a questão

---
+++ lista02/lista02_exercicio07.py
@@ -0,0 +1,21 @@
+#-----------------------------------------------------------------------------------------------------------------------
+#Introdução a Programação de Computadores - IPC
+#Universidade do Estado do Amazonas - UEA
+#Prof. Jucimar Jr.
+#Alexandre Marques Uchôa               1715310028
+#Jandinne Duarte de Oliveira           1015070265
+#Uriel Brito Barros                    1515120558
+#Roberta de Oliveira da cruz           0825070169
+#Evandro Padilha Barroso Filho         1715310009
+#
+##
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+#-----------------------------------------------------------------------------------------------------------------------
+import turtle
+t = turtle.Pen()
+for x in range(100):
+    t.fd(x)
+    t.lt(60)
+
+
+
