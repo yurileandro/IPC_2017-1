9e8a4b9fd9d4047845fa8e4dbfdb1a788262d517
EnriqueIzel2 <eleaoizel8@gmail.com>
2017-03-14 10:40:13 -0400

    Add files via upload

---
+++ lista02/lista02_exercicio02.py.py
@@ -0,0 +1,23 @@
+﻿#---------------------------------------------------------------------
+#Introdução a Programação dos Computadores - IPC
+#Prof. Jucimar Jr.
+#Adham Lucas da Silva Oliveira           1715310001
+#Enrique Leão Barbosa Izel               1715310048
+#Erik Atilio Silva Rey                   1715310059
+#Ulisses Antonio Antonino da Costa       1515090555
+#Desenhar um polígono com 3 lados iguais. Cada lado uma cor diferente.
+
+import turtle  
+t = turtle.Pen() 
+turtle.bgcolor("purple") 
+for x in range(1):
+    t.pensize(20) 
+    t.pencolor("red")
+    t.forward(200) 
+    t.left(120) 
+    t.pencolor("blue")
+    t.forward(200) 
+    t.left(120) 
+    t.pencolor("black")
+    t.forward(200) 
+    t.left(120) 
