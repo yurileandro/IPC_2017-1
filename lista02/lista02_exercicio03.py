822a0ecc05f6303b7f473ccaf8c8dd0fc1061a0e
Felipe Almeida <edu468felipe@gmail.com>
2017-03-14 17:51:39 -0400

    Update lista02_exercicio03.py
    Atualizando informações no código. Obs: Outro participante do meu grupo vai concluir o código.

--- lista02/lista02_exercicio03.py
+++ lista02/lista02_exercicio03.py
@@ -5,7 +5,7 @@
 # Aracille de Souza Barbosa           1315120206
 # Felipe Eduardo Silva de Almeida     1715310031
 # Kethelen Tamara Braga               1525212002
-# Naiara da Silva Cedeira da Costa    1715310038
+# Nayara da Silva Cedeira da Costa    1715310038
 # Vitor Summer Oliveira Pantaleão     1715310042
 # Yuri Leandro de Aquino Silva        1615100462
 #
@@ -15,6 +15,6 @@
 
 import turtle # importa a biblioteca turtle
 colors=['red', 'purple', 'blue','green', 'yellow'] #cria um vetor de 5 posições do tipo string para cada lado do polígono
-t=turtle.Pen() #
+t=turtle.Pen() # intancia a janela turtle
 turtle.bgcolor('black') # muda a cor doefundo da janela para preto
 for x in range(5): # inicia um laço de 0 até 4 para a criação de cada lado do poligno
