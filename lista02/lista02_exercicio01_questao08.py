7048baf64376cdef54cd9d1140655d91a89b0c73
yurileandro <yurileandro0123@outlook.com>
2017-03-14 21:24:06 -0400

    Create lista02_exercicio01_questao08.py
    A questão ficou de ser completada por outro integrante do grupo.

---
+++ lista02/lista02_exercicio01_questao08.py
@@ -0,0 +1,16 @@
+# ----------------------------------------------------------
+# Introdução a Programação de Computadores - IPC
+# Universidade do Estado do Amazonas - UEA
+# Prof. Jucimar Jr
+# Aracille de Souza Barbosa           1315120206
+# Felipe Eduardo Silva de Almeida     1715310031
+# Kethelen Tamara Braga               1525212002
+# Nayara da Silva Cedeira da Costa    1715310038
+# Vitor Summer Oliveira Pantaleão     1715310042
+# Yuri Leandro de Aquino Silva        1615100462
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+#
+#-----------------------------------------------------------
+
+v=int(input("Quanto você ganha por hora?"))
+n=int(input("Quantas horas você trabalha no mês?"))
